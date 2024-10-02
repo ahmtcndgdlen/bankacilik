f86f3fbbfa1677575260aba432c400a483d2f132
ahmetcan dağdelen <ahmtcndgdlen@gmail.com>
2024-10-02 14:09:35 +0300

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Random rand = new Random();
+                Console.WriteLine("akbank");
+
+                bankaHesabı hesap = new bankaHesabı
+                {
+                    bankaHesab = rand.Next(0, 100000),
+                    kullaniciAdi = "ahmet",
+                    kullaniciSoyadi = "dağdelen"
+                };
+                Console.WriteLine($"{hesap.kullaniciAdi} {hesap.kullaniciSoyadi} {hesap.bankaHesab}");
+
+                Console.WriteLine(" yapacağınız işlemi seçin \n 1: para çekme. \n 2: para yatırma. \n 3: kalan bakiyeyi göster");
+
+                int secim = Convert.ToInt32( Console.ReadLine() );
+                if (secim == 1)
+                {
+                    Console.WriteLine("çekmek istediğiniz tutarı girin");
+                    var cekilecekTutar = Convert.ToInt32(Console.ReadLine());
+                    hesap.ParaCek(cekilecekTutar);
+                }
+                else if (secim == 2)
+                {
+                    Console.WriteLine("yatırmak istediğiniz tutarı girin");
+                    var yatirilacakTutar = Convert.ToInt32(Console.ReadLine());
+                    hesap.Parayatir(yatirilacakTutar);
+
+                }
+                else if (secim == 3)
+                {
+                    Console.WriteLine($"kalan bakiye: {hesap.bankaHesab}");
+                }
+                else { Console.WriteLine("geçersiz seçim"); }
+
+
+                Console.WriteLine($"kalan bakiye: {hesap.bankaHesab}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp17/bankaHesabı.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp17
+{
+    internal class bankaHesabı
+    {
+        public int bankaHesab { get; set; }
+        public string kullaniciAdi {  get; set; }
+        public string kullaniciSoyadi { get; set; }
+
+        internal void ParaCek(int cekilecekTutar)
+        {
+            bankaHesab -= cekilecekTutar;
+        }
+
+        internal void Parayatir(int yatirilacakTutar)
+        {
+            bankaHesab += yatirilacakTutar;
+        }
+    }
+}
